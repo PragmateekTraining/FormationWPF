d55bf2988808296755fd3fd65ff90582fa0175cc
Pragmateek <postmaster@pragmateek.com>
2014-04-20 20:07:34 +0200

    Enhance custom control

---
+++ GradeChooser/IsGreaterOrEqualConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GradeChooser
+{
+    public class IsGreaterOrEqualConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int valueAsInt = System.Convert.ToInt32(value);
+            int parameterAsInt = System.Convert.ToInt32(parameter);
+
+            return valueAsInt >= parameterAsInt;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
