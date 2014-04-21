3d1572fcbd94ceb43ab7af0d02f23e3b68c60ee8
Pragmateek <postmaster@pragmateek.com>
2014-04-21 23:01:16 +0200

    Random markup extension

---
+++ Xaml/RandomExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Xaml
+{
+    public class RandomExtension : MarkupExtension
+    {
+        private static readonly Random rand = new Random();
+
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+
+        public RandomExtension()
+        {
+        }
+
+        public RandomExtension(int min)
+        {
+            Min = min;
+        }
+
+        public RandomExtension(int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return rand.Next(Min, Max);
+        }
+    }
+}
