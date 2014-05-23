0b1dae98aadf325ddcc3a6ae5620e2b7602fa79d
Pragmateek <postmaster@pragmateek.com>
2014-05-24 00:43:16 +0200

    More samples

---
+++ WindowsAndMessageBoxes/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsAndMessageBoxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            MessageBox.Show("Bye " + Environment.UserName);
+
+            base.OnClosed(e);
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            MessageBox.Show("Hello " + Environment.UserName);
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void toto_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsAndMessagesBoxesFromScratch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WindowsAndMessagesBoxesFromScratch
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application application = new Application();
+
+            Window mainWindow = new Window();
+            mainWindow.Loaded += (s, a) => MessageBox.Show("Hello " + Environment.UserName);
+            mainWindow.Closed += (s, a) => MessageBox.Show("Bye " + Environment.UserName);
+
+            mainWindow.Show();
+
+            application.MainWindow = mainWindow;
+
+            application.Run();
+        }
+    }
+}
