b8bdf82bc66bda255511873d1962b9cecf28a4ff
Pragmateek <postmaster@pragmateek.com>
2014-05-27 01:12:38 +0200

    Still more samples

---
+++ Scrolling/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scrolling
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            const int n = 100;
+
+            for (int i = 0; i < n; ++i)
+            {
+                list1.Items.Add(i);
+                list2.Items.Add(i);
+                list3.Items.Add(i);
+            }
+        }
+    }
+}
