8e81a11ac196a36a19b7b613c444ef2b65aff58d
Pragmateek <postmaster@pragmateek.com>
2014-05-30 01:17:15 +0200

    More samples

--- ArrayElementBinding/MainWindow.xaml.cs
+++ ArrayElementBinding/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SystemResources
+namespace ArrayElementBinding
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- VisualParentUnicity/MainWindow.xaml.cs
+++ VisualParentUnicity/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,15 @@ namespace VisualParentUnicity
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            PropertyChanged(this, new PropertyChangedEventArgs("SomePropertyName"))
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
