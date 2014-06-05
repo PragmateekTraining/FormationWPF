edc30851a6d63fa769ee1417e398dcf4cdc84654
Pragmateek <postmaster@pragmateek.com>
2014-06-06 01:25:08 +0200

    More samples

---
+++ AttachedBehavior/KikooEffects.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace AttachedBehavior
+{
+    public class KikooEffects
+    {
+        public static bool GetIsBlinking(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsBlinkingProperty);
+        }
+
+        public static void SetIsBlinking(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsBlinkingProperty, value);
+        }
+
+        public static readonly DependencyProperty IsBlinkingProperty =
+            DependencyProperty.RegisterAttached("IsBlinking", typeof(bool), typeof(KikooEffects), new PropertyMetadata(false, OnBlinkValueChanged));
+
+        static readonly IDictionary<DependencyObject, Storyboard> storyboards = new Dictionary<DependencyObject, Storyboard>();
+
+        private static void OnBlinkValueChanged(DependencyObject target, DependencyPropertyChangedEventArgs args)
+        {
+            if ((bool)args.NewValue)
+            {
+                if (!storyboards.ContainsKey(target))
+                {
+                    DoubleAnimation animation = new DoubleAnimation
+                    {
+                        Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+                        To = 0
+                    };
+
+                    Storyboard.SetTarget(animation, target);
+                    Storyboard.SetTargetProperty(animation, new PropertyPath("Opacity"));
+
+                    Storyboard storyboard = new Storyboard
+                    {
+                        AutoReverse = true,
+                        RepeatBehavior = RepeatBehavior.Forever,
+                    };
+
+                    storyboard.Children.Add(animation);
+
+                    storyboards[target] = storyboard;
+                }
+
+                storyboards[target].Begin();
+            }
+            else
+            {
+                Storyboard storyboard = null;
+
+                if (storyboards.TryGetValue(target, out storyboard))
+                {
+                    storyboard.Stop();
+                }
+            }
+        }
+    }
+}
--- ControlTemplate/MainWindow.xaml.cs
+++ ControlTemplate/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ControlTemplate
 {
@@ -83,7 +72,7 @@ namespace ControlTemplate
         public object ConvertBack(object value,
             Type targetType,
             object parameter,
-            System.Globalization.CultureInfo culture)
+            CultureInfo culture)
         {
             return DependencyProperty.UnsetValue;
         }
