4ac5cc894a4ffd323c2a49b418f61679c16f5263
Pragmateek <postmaster@pragmateek.com>
2014-06-15 17:10:11 +0200

    More samples

---
+++ CanvasSample/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CanvasSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            availableBrushes = typeof(Brushes).GetProperties().Select(pi => pi.GetValue(null, null) as Brush).ToArray();
+        }
+
+        private readonly Random rand = new Random();
+
+        private Brush[] availableBrushes = null;
+
+        private Brush GetRandomBrush()
+        {
+            return availableBrushes[rand.Next(availableBrushes.Length)];
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock tb = new TextBlock
+            {
+                Text = "" + (char)(rand.Next(256 - 33) + 33),
+                Foreground = GetRandomBrush(),
+                FontWeight = FontWeights.Bold
+            };
+            tb.SetValue(Canvas.TopProperty, e.GetPosition(canvas).Y);
+            Canvas.SetLeft(tb, e.GetPosition(canvas).X);
+
+            canvas.Children.Add(tb);
+        }
+    }
+}
---
+++ MediaElementSample/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaElementSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isPaused = true;
+
+        const string imagesUriTemplate = "pack://application:,,,/Assets;component/images/icons/{0}.png";
+
+        ImageSource playSource;
+        ImageSource pauseSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TypeConverter converter = new ImageSourceConverter();
+            playSource = converter.ConvertFrom(new Uri(string.Format(imagesUriTemplate, "play"))) as ImageSource;
+            pauseSource = converter.ConvertFrom(new Uri(string.Format(imagesUriTemplate, "pause"))) as ImageSource;
+
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            image.Source = isPaused ? playSource : pauseSource;
+        }
+
+        private void video_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            TogglePlayPause();
+        }
+
+        private void image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            TogglePlayPause();
+        }
+
+        private void TogglePlayPause()
+        {
+            if (isPaused)
+                video.Play();
+            else
+                video.Pause();
+
+            isPaused = !isPaused;
+
+            UpdateImage();
+        }
+    }
+}
