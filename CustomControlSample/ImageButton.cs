0fd6eeab653a0dd034a41edf320bd76c7ed92ef4
Pragmateek <training@pragmateek.com>
2014-08-08 17:14:03 +0200

    Add ImageButton control sample

---
+++ CustomControlSample/ImageButton.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CustomControlSample
+{
+    public class ImageButton : Button
+    {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(OnImagePropertyChanged));
+        public static readonly DependencyProperty ImageWidthProperty = DependencyProperty.Register("ImageWidth", typeof(int), typeof(ImageButton), new PropertyMetadata(OnImagePropertyChanged));
+        public static readonly DependencyProperty ImageHeightProperty = DependencyProperty.Register("ImageHeight", typeof(int), typeof(ImageButton), new PropertyMetadata(OnImagePropertyChanged));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public int ImageWidth
+        {
+            get { return (int)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+        
+        public int ImageHeight
+        {
+            get { return (int)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }        
+
+        private bool updatingContent = false;
+
+        public ImageButton()
+        {
+            HorizontalContentAlignment = System.Windows.HorizontalAlignment.Stretch;
+        }
+
+        private static void OnImagePropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs args)
+        {
+            (source as ImageButton).UpdateImage();
+        }
+
+        private Image image = null;
+
+        private void UpdateImage()
+        {
+            if (image != null)
+            {
+                image.Source = Image;
+                image.Width = ImageWidth;
+                image.Height = ImageHeight;
+                image.Margin = new Thickness(0, 0, 5, 0);
+            }
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            if (!updatingContent)
+            {
+                updatingContent = true;
+
+                try
+                {
+                    DockPanel panel = new DockPanel();
+
+                    image = new Image();
+
+                    image.SetValue(DockPanel.DockProperty, Dock.Left);
+                    image.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+
+                    UpdateImage();
+
+                    FrameworkElement originalContent = newContent is string ? new TextBlock { Text = newContent as string } :
+                                                       newContent as FrameworkElement;
+
+                    originalContent.VerticalAlignment = VerticalAlignment.Center;
+
+                    panel.Children.Add(image);
+                    panel.Children.Add(originalContent);
+
+                    Content = panel;
+                }
+                finally
+                {
+                    updatingContent = false;
+                }
+            }
+        }
+    }
+}
---
+++ CustomControlSample/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CustomControlSample
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer == null)
+            {
+                timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(300) };
+                timer.Tick += delegate { track.Text += "Boom! "; };
+            }
+
+            timer.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
