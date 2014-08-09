92817fc76efd96a6c1fea8bb4bcdcee69268d01f
Pragmateek <training@pragmateek.com>
2014-08-09 19:15:17 +0200

    Add DispatcherTimer and transforms sample

---
+++ DispatcherTimerSample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DispatcherTimerSample
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        Timer badTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs args)
+        {
+            timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += delegate { UpdateClock(); };
+            timer.Start();
+
+            badTimer = new Timer { Interval = 1000 };
+            badTimer.Elapsed += delegate { UpdateClock(); };
+            badTimer.Start();
+
+            UpdateClock();
+        }
+
+        void UpdateClock()
+        {
+            DateTime now = DateTime.Now;
+
+            hourHandRotation.Angle = (now.Hour % 12) * 360.0 / 12 + 1.0 * now.Minute / 60 * 1.0 / 12  + 1.0 * now.Second / 60 * 1.0 / 60;
+            minuteHandRotation.Angle = now.Minute * 360.0 / 60 + 1.0 * now.Second / 60 * 1.0 / 60;
+            secondHandRotation.Angle = now.Second * 360.0 / 60;
+        }
+    }
+}
