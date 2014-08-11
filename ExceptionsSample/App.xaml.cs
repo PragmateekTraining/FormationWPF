2bce547390f8e42f781536e8dcb21632d38d63a1
Pragmateek <training@pragmateek.com>
2014-08-11 23:20:11 +0200

    Add Exceptions handling and static binding samples

---
+++ ExceptionsSample/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExceptionsSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static bool HandleThem { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            this.DispatcherUnhandledException += (s, a) =>
+                {
+                    if (HandleThem)
+                    {
+                        a.Handled = true;
+
+                        MessageBox.Show(string.Format("Got one: {0}!", a.Exception));
+                    }
+                };
+        }
+    }
+}
