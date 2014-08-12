4fb65672a3760774f79565f0d10d10442f10b002
Pragmateek <training@pragmateek.com>
2014-08-12 15:34:11 +0200

    Add MultiBinding sample

---
+++ MultiBindingSample/SumConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace MultiBindingSample
+{
+    public class SumConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ChangeType(values.Sum(v => System.Convert.ToDecimal(string.IsNullOrWhiteSpace(v.ToString()) ? 0 : v)), targetType);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
