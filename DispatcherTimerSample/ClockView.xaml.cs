d1576937d040817e42b2ccfcfc1d5f48cb70904c
Pragmateek <training@pragmateek.com>
2014-08-13 15:34:54 +0200

    Refactor DispatcherTimer sample to be MVVM compliant

---
+++ DispatcherTimerSample/ClockView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DispatcherTimerSample
+{
+    public partial class ClockView : UserControl
+    {
+        public ClockViewModel Model
+        {
+            get { return (ClockViewModel)GetValue(ModelProperty); }
+            set { SetValue(ModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ModelProperty = DependencyProperty.Register("Model", typeof(ClockViewModel), typeof(ClockView), new PropertyMetadata(OnModelChanged));
+
+        public ClockView()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnModelChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ClockView clockView = sender as ClockView;
+            ClockViewModel model = args.NewValue as ClockViewModel;
+
+            model.PropertyChanged += (s, a) =>
+                {
+                    if (a.PropertyName == "Time")
+                    {
+                        // Snapshot the latest time
+                        DateTime now = model.Time;
+
+                        clockView.hourHandRotation.Angle = (now.Hour % 12) * 360.0 / 12 + 1.0 * now.Minute / 60 * 1.0 / 12 + 1.0 * now.Second / 60 * 1.0 / 60;
+                        clockView.minuteHandRotation.Angle = now.Minute * 360.0 / 60 + 1.0 * now.Second / 60 * 1.0 / 60;
+                        clockView.secondHandRotation.Angle = now.Second * 360.0 / 60;
+                    }
+                };
+
+            model.Run();
+        }
+    }
+}
---
+++ DispatcherTimerSample/ClockViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Timers;
+using System.Windows.Threading;
+
+namespace DispatcherTimerSample
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        DispatcherTimer timer;
+        Timer badTimer;
+
+        private DateTime time;
+        public DateTime Time
+        {
+            get { return time; }
+            set
+            {
+                if (value != time)
+                {
+                    time = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Time"));
+                }
+            }
+        }
+
+        public void Run()
+        {
+            timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += delegate { Time = DateTime.Now; };
+            timer.Start();
+
+            badTimer = new Timer { Interval = 1000 };
+            badTimer.Elapsed += delegate { Time = DateTime.Now; };
+            badTimer.Start();
+
+            Time = DateTime.Now;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
