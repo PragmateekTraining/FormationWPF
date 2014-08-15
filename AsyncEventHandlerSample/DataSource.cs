e9a091355e1e054e2302e24074ff3b76f8eaf6ef
Pragmateek <training@pragmateek.com>
2014-08-16 00:56:51 +0200

    Add async event handler sample

---
+++ AsyncEventHandlerSample/DataSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncEventHandlerSample
+{
+    public class DataSource
+    {
+        public static async Task<string> GetNextDataAsync()
+        {
+            await Task.Delay(1000);
+
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ AsyncEventHandlerSample/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace AsyncEventHandlerSample
+{
+    public partial class MainWindow : Window
+    {
+        bool isCanceled;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void loadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            loadDataButton.IsEnabled = false;
+            cancelButton.IsEnabled = true;
+            progressMarker.IsActive = true;
+            
+            while (!isCanceled)
+            {
+                string data = await DataSource.GetNextDataAsync();
+
+                dataOutput.Text += data + "\n";
+
+                scrollViewer.ScrollToBottom();
+            }
+
+            loadDataButton.IsEnabled = true;
+            progressMarker.IsActive = false;
+            isCanceled = false;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            isCanceled = true;
+
+            cancelButton.IsEnabled = false;
+        }
+    }
+}
