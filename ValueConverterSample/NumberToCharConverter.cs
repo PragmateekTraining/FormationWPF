92629ae613bb0d88eeaa3d61df85955b69d531a5
Pragmateek <training@pragmateek.com>
2014-08-28 12:23:57 +0200

    Add full bidirectional value converter sample

---
+++ ValueConverterSample/NumberToCharConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace ValueConverterSample
+{
+    public class NumberToCharConverter : IValueConverter
+    {
+        public char? Character { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string parameterAsString = parameter as string;
+
+            char? localValue = parameterAsString != null && parameterAsString.Length >= 1 ? parameterAsString[0] : null as char?;
+
+            char character = localValue ?? Character ?? '*';
+
+            int number = System.Convert.ToInt32(value);
+
+            return new string(character, number);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string s = value as string;
+
+            return s.Length;
+        }
+    }
+}
