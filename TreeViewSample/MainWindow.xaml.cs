03bbd606d26cfd56dc1175d4d591a1879c76dbfd
Pragmateek <training@pragmateek.com>
2014-10-03 00:06:52 +0200

    Add Expander's header RotateTransform sample

--- TreeViewSample/MainWindow.xaml.cs
+++ TreeViewSample/MainWindow.xaml.cs
@@ -60,9 +60,6 @@ namespace TreeViewSample
         }
     }
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
